ffd1bba93aeed40d4641b7b53746fae7f6c70b06
Shih Hao Lin <only520you2002@gmail.com>
2016-02-16 08:14:43 +0800

    安裝XLSX 並測試

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -5,16 +5,30 @@ var upload = multer({
     dest: './uploads/'
 }).single('studentList');
 var fs = require('fs');
+var XLSX = require('xlsx');
 
 router.post('/student_upload', function(req, res, next) {
     upload(req, res, function(err) {
         if (err) {
             console.log(err);
+            res.json({
+                status: 0
+            });
         }
-        readStudentsList(req.file.path);
-        res.json({
-            id: 1
-        });
+
+        var ext = getFileExt(req.file.originalname);
+        if (ext != "xlsx" || ext != 'xls'){
+            res.json({
+                stauts: 0,
+                msg: '僅接受xlsx, xls, csv'
+            })
+        }
+        
+        // console.log(req.file.originalname);
+        readStudentsList(req.file.path, ext);
+        // res.json({
+        //     id: 1
+        // });
     });
 });
 
@@ -24,14 +38,32 @@ router.post('/student_upload', function(req, res, next) {
     });
 });
 
-function readStudentsList(path) {
-    var lineReader = require('readline').createInterface({
-        input: fs.createReadStream(path)
-    });
+function readStudentsList(path, ext) {
 
-    lineReader.on('line', function(line) {
-        console.log('Line from file:', line);
-    });
+    if(ext == 'xlsx' || ext == 'xls'){
+        var workbook = XLSX.readFile(path, {type:"binary"});
+        console.log(workbook);
+    }
+
+
+
+    // var lineReader = require('readline').createInterface({
+    //     input: fs.createReadStream(path)
+    // });
+
+    // lineReader.on('line', function(line) {
+    //     console.log('Line from file:', line);
+    // });
+}
+
+function getFileExt(filename){
+    var extIndex = filename.lastIndexOf('.');
+    var ext;
+    if (extIndex != -1) 
+    {
+       ext= filename.substr(extIndex+1, filename.length);  //副檔名
+    }
+    return ext
 }
 
 module.exports = router;
